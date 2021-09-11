6a98871e601d16e08fba4cd815a7df24982e6f7b
JoJo Myles <jo.myles3112@gmail.com>
2021-09-11 10:13:48 -0400

    fixed the path

--- GradeManager/Program.cs
+++ GradeManager/Program.cs
@@ -6,18 +6,27 @@ namespace GradeManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Grade Manager");
+            //Console.WriteLine("Grade Manager");
 
-            Console.Write("Please Select a Number 1-8: ");
-            Console.ReadLine();
+            //Console.Write("Please Select a Number 1-8: ");
+            //Console.ReadLine();
+            MainMenu();
         }
         static void MainMenu()
         {
-
+            Console.WriteLine("Welcome to My Grade Manager!");
+            Console.WriteLine("1. Add Grades");
+            Console.WriteLine("2. Edit Grades");
+            Console.WriteLine("3. Show Grades");
+            Console.WriteLine("4. Lowest Grade");
+            Console.WriteLine("5. Highest Grade");
+            Console.WriteLine("6. Exit");
+            Console.ReadLine();
         }
 
         static void ShowGrades()
         {
+            Console.WriteLine("");
 
         }
 
