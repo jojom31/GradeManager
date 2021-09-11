428c0ae84c9eca299daa865a38d6617498abd108
JoJo Myles <jo.myles3112@gmail.com>
2021-09-11 09:31:16 -0400

    adding features to lab assignment

---
+++ GradeManager/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GradeManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grade Manager");
+
+            Console.Write("Please Select a Number 1-8: ");
+            Console.ReadLine();
+        }
+        static void MainMenu()
+        {
+
+        }
+
+        static void ShowGrades()
+        {
+
+        }
+
+        static void EditGrades()
+        {
+
+        }
+
+        static void AddGrades()
+        {
+
+        }
+
+
+
+    }
+}
