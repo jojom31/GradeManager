74943628011a13d76e9cc17d40f67a95fbf82f87
JoJo Myles <jo.myles3112@gmail.com>
2021-09-11 20:54:18 -0400

    creating the add grade feature

--- GradeManager/Program.cs
+++ GradeManager/Program.cs
@@ -14,6 +14,7 @@ namespace GradeManager
         }
         static void MainMenu()
         {
+            Console.Clear();
             Console.WriteLine("Welcome to My Grade Manager!");
             Console.WriteLine("1. Add Grades");
             Console.WriteLine("2. Edit Grades");
@@ -21,26 +22,78 @@ namespace GradeManager
             Console.WriteLine("4. Lowest Grade");
             Console.WriteLine("5. Highest Grade");
             Console.WriteLine("6. Exit");
-            Console.ReadLine();
+
+            string myMenu = Console.ReadLine();
+
+            switch (myMenu)
+            {
+                case "1":
+                    AddGrades();
+                    break;
+                case "2":
+                    EditGrades();
+                        break;
+                case "3":
+                    ShowGrades();
+                    break;
+                case "4":
+                    LowestGrades();
+                    break;
+                case "5":
+                    HighestGrades();
+                    break;
+                case "6":
+                    Exit();
+                    break;
+
+            }
+
+
+            //
+            MainMenu();
         }
 
-        static void ShowGrades()
+        static void AddGrades()
         {
-            Console.WriteLine("");
+            Console.WriteLine("Please add grade");
+            int grade = int.Parse(Console.ReadLine());
+            Console.WriteLine(grade);
+            Console.ReadLine();
 
         }
 
         static void EditGrades()
         {
+            Console.WriteLine("edit grades");
+            Console.ReadLine();
 
         }
 
-        static void AddGrades()
+        static void ShowGrades()
         {
-
+            Console.WriteLine("show grades");
+            Console.ReadLine();
         }
 
+        static void LowestGrades()
+        {
+            Console.WriteLine("highest grades");
+            Console.ReadLine();
+        }
 
+        static void HighestGrades()
+        {
+            Console.WriteLine("Highest grades");
+            Console.ReadLine();
+        }
+
+        static void Exit()
+        {
+            Console.WriteLine("exit");
+            Console.ReadLine();
+        }
+
+        
 
     }
 }
